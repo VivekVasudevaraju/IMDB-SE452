738e75b2bde52abeba721ccc579b641dcb6ca061
Vivek Raju <vivekraju@Viveks-MacBook-Air.local>
2022-05-25 23:23:35 -0500

    New header bar

--- src/main/react-web/src/pages/HeaderBar.jsx
+++ src/main/react-web/src/pages/HeaderBar.jsx
@@ -1,27 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import UserCard from "../components/User/UserCard";
-import Header from "../components/Header";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { BsSearch } from "react-icons/bs";
 
 const HeaderBar = () => {
   return (
     <Navbar bg="dark" expand="lg">
-      <Container>
-        <Navbar.Brand>
-          <Header text="IMDB" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/movies" className="btn btn-dark">
-              Movies
-            </Link>
-          </Nav>
-          <React.Suspense fallback="Loading...">
-            <UserCard />
-          </React.Suspense>
+      <Container fluid>
+        <Navbar.Brand href="#">IMDB</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success d-flex align-items-center">
+              <BsSearch />
+            </Button>
+          </Form>
         </Navbar.Collapse>
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: "100px" }}
+          navbarScroll
+        >
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">Link</Nav.Link>
+          <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#" disabled>
+            Link
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
