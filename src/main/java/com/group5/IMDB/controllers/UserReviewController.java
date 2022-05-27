c54a3f93b55342c219d7c3762a41ceb6e4960f19
Leo D Penrose <lpenrose@depaul.edu>
2022-05-26 23:29:50 -0500

    updated user review controller

--- src/main/java/com/group5/IMDB/controllers/UserReviewController.java
+++ src/main/java/com/group5/IMDB/controllers/UserReviewController.java
@@ -1,4 +1,16 @@
 package com.group5.IMDB.controllers;
 
+import com.group5.IMDB.services.UserReviewService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
 public class UserReviewController {
+
+    private final UserReviewService userReviewService;
+
+    public UserReviewController(UserReviewService userReviewService) {
+        this.userReviewService = userReviewService;
+    }
 }
