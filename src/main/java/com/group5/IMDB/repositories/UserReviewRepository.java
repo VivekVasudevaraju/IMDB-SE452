b8ed3951a4e7596eff226253edc3e0a6a03da2eb
Leo D Penrose <lpenrose@depaul.edu>
2022-05-26 22:43:45 -0500

    updated repo and service

--- src/main/java/com/group5/IMDB/repositories/UserReviewRepository.java
+++ src/main/java/com/group5/IMDB/repositories/UserReviewRepository.java
@@ -1,6 +1,9 @@
 package com.group5.IMDB.repositories;
 
-public interface UserReviewRepository {
+import com.group5.IMDB.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReviewRepository extends JpaRepository<Review,Long> {
 
     //PlaceHolder For Repo Class
 
--- src/main/java/com/group5/IMDB/services/UserReviewService.java
+++ src/main/java/com/group5/IMDB/services/UserReviewService.java
@@ -1,8 +1,14 @@
 package com.group5.IMDB.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserReviewService {
 
-    //Placeholder for Service
+    private final UserReviewService userReviewService;
 
+    @Autowired
+    public UserReviewService(UserReviewService userReviewService) {
+        this.userReviewService = userReviewService;
+    }
 }
 
