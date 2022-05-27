5b3b6192feb5d97c4daef6388ee95bb8d8616dad
Leo D Penrose <lpenrose@depaul.edu>
2022-05-27 00:18:58 -0500

    updated controller to show all user reviews

--- src/main/java/com/group5/IMDB/controllers/UserReviewController.java
+++ src/main/java/com/group5/IMDB/controllers/UserReviewController.java
@@ -1,8 +1,13 @@
 package com.group5.IMDB.controllers;
 
+import com.group5.IMDB.entities.Review;
 import com.group5.IMDB.services.UserReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/review")
@@ -13,4 +18,12 @@ public class UserReviewController {
     public UserReviewController(UserReviewService userReviewService) {
         this.userReviewService = userReviewService;
     }
+
+    @RequestMapping("/userreviews")
+    public ResponseEntity<List<Review>> getAllReviews(){
+        List<Review> reviews = userReviewService.getAllReviews();
+        System.out.println(reviews);
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
+
 }
