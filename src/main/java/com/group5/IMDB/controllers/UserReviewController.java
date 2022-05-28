a7463735188ae29157884c95ac3a62959eaff5c7
Leo D Penrose <lpenrose@depaul.edu>
2022-05-28 01:19:24 -0500

    updated controller,repo,service, review jsx, reviewPage jsx

--- src/main/java/com/group5/IMDB/controllers/UserReviewController.java
+++ src/main/java/com/group5/IMDB/controllers/UserReviewController.java
@@ -4,6 +4,8 @@ import com.group5.IMDB.entities.Review;
 import com.group5.IMDB.services.UserReviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,12 @@ public class UserReviewController {
         List<Review> reviews = userReviewService.getAllReviews();
         System.out.println(reviews);
         return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
+
+    @GetMapping("find/{id}")
+    public ResponseEntity<Review> getUserReviewById(@PathVariable("id") Long id){
+        Review userReview = userReviewService.getUserReview(id);
+        return  new ResponseEntity<>(userReview,HttpStatus.OK);
     }
 
 }
