fb4ae56b17e040146cec991ebd088a285d8b0540
Leo D Penrose <lpenrose@depaul.edu>
2022-06-04 05:06:55 -0500

    fix review submit bug

--- src/main/react-web/src/pages/UserReviewPage.jsx
+++ src/main/react-web/src/pages/UserReviewPage.jsx
@@ -5,7 +5,7 @@ import {useParams} from "react-router-dom";
 
 function UserReviewPage() {
     const {theMovieId} = useParams()
-    const apiURL = "api/review/add/"
+    const apiURL = "/api/review/add/"
     const [myReviewTitle,setMyReviewTitle] = useState("")
     const [myReviewData,setMyReviewData] = useState("")
     const [myMovieData,setMovieData] = useState( [])
