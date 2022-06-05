34e0eeddafa01b1df42eb31dc71bdc4e7cf4f725
Leo D Penrose <lpenrose@depaul.edu>
2022-06-04 20:48:11 -0500

    fixed Movie conflict

--- src/main/java/com/group5/IMDB/entities/Movie.java
+++ src/main/java/com/group5/IMDB/entities/Movie.java
@@ -8,13 +8,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "movies")
