d3d164ad467945c4506ffc1165e41431b581d855
Vivekraju <rapstar.vicks@gmail.com>
2022-06-05 11:44:01 -0500

    Entitiy update for booking

--- src/main/java/com/group5/IMDB/entities/Theatre.java
+++ src/main/java/com/group5/IMDB/entities/Theatre.java
@@ -1,8 +1,13 @@
 package com.group5.IMDB.entities;
 
-
 import lombok.Data;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.*;
 
 @Entity
@@ -12,19 +17,25 @@ import javax.persistence.*;
 public class Theatre {
 
     @Id
-    @Column(name = "theatre_id",columnDefinition = "bigint")
+    @Column(name = "theatre_id", columnDefinition = "bigint")
     private Long theatreId;
 
-    @Column(name = "theatre_name" ,columnDefinition = "varchar(25)")
+    @Column(name = "theatre_name", columnDefinition = "varchar(25)")
     private String theatreName;
 
-    @Column(name = "theatre_zipcode" ,columnDefinition = "int")
+    @Column(name = "theatre_zipcode", columnDefinition = "int")
     private Integer theatreZipcode;
 
-    @Column(name = "theatre_city" ,columnDefinition = "varchar(25)")
+    @Column(name = "theatre_city", columnDefinition = "varchar(25)")
     private String theatreCity;
 
-    @Column(name = "theatre_state" ,columnDefinition = "varchar(25)")
+    @Column(name = "theatre_state", columnDefinition = "varchar(25)")
     private String theatreState;
 
+    @OneToMany // (fetch = FetchType.EAGER)
+    @JoinTable(name = "theatre_auditorium", joinColumns = {
+            @JoinColumn(name = "theatre_id", referencedColumnName = "theatre_id") }, inverseJoinColumns = {
+                    @JoinColumn(name = "auditorium_id", referencedColumnName = "auditorium_id", unique = true)
+            })
+    private List<Auditorium> theatre_auditoriums;
 }
