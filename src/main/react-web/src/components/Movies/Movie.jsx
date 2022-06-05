5d7c0b799ca8c076b38f7428f9098ce2e1dc5ab8
kvssrohit <60591737+kvssrohit@users.noreply.github.com>
2022-06-05 12:38:36 -0500

    Implemented the rating in homepage

--- src/main/react-web/src/components/Movies/Movie.jsx
+++ src/main/react-web/src/components/Movies/Movie.jsx
@@ -5,25 +5,42 @@ import "./Movie.css";
 import { Rating } from "react-simple-star-rating";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-const Movie = ({ title, imageUrl, rating, movieId }) => {
-  const history = useHistory()
+const Movie = ({ movieData }) => {
+  const history = useHistory();
+
+  function calculateRating() {
+    let ratingTotal = 0;
+    const { ratings } = movieData;
+
+    ratings.forEach((rating) => {
+      const { ratingLevel } = rating;
+      ratingTotal += ratingLevel;
+    });
+
+    const avgRating = ratingTotal / ratings.length / 10;
+    return Math.round(avgRating * 10) / 10;
+  }
+
   return (
-    <Card style={{ width: "12em", margin: "0em 1em 1em 0em", cursor: "pointer" }} onClick={() => history.push(`/movie/${movieId}`)}>
-      <Card.Img variant="top" src={imageUrl} />
+    <Card
+      style={{ width: "12em", margin: "0em 1em 1em 0em", cursor: "pointer" }}
+      onClick={() => history.push(`/movie/${movieData.movieId}`)}
+    >
+      <Card.Img variant="top" src={movieData.movieImg} />
       <Card.Body>
-        <Card.Title style={{ fontSize: "1.0rem" }}>{title}</Card.Title>
+        <Card.Title style={{ fontSize: "1.0rem" }}>
+          {movieData.movieTitle}
+        </Card.Title>
         <Rating
-          ratingValue={rating}
+          ratingValue={100}
           emptyColor="#f5c518"
           iconsCount={1}
+          size={25}
           readonly="true"
         />
-        <span className="card-rating" style={{ fontSize: "1rem" }}>
-          {rating}
-        </span>
+        <span className="card-rating">{calculateRating()}</span>
       </Card.Body>
     </Card>
   );
 };
-
 export default Movie;
